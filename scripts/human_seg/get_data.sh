bc859a797e2a0bcabf3a4fe3f33ca1599e45b94e
Nazmi <nazmiryuki@gmail.com>
2025-06-16 23:28:11 +0800

    u

--- scripts/human_seg/get_data.sh
+++ scripts/human_seg/get_data.sh
@@ -6,4 +6,4 @@ DATADIR='datasets' #location where data gets downloaded to
 echo "downloading the data and putting it in: " $DATADIR
 mkdir -p $DATADIR && cd $DATADIR
 wget https://www.dropbox.com/s/s3n05sw0zg27fz3/human_seg.tar.gz
-tar -xzvf human_seg.tar.gz && rm human_seg.tar.gz
+tar -xzvf human_seg.tar.gz --no-same-owner && rm human_seg.tar.gz
